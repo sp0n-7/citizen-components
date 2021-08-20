9ceb99d8f669cbace393ceae118aab26bd4682f9
Kushan <kushan.gupta@citizen.com>
2021-08-20 14:21:19 -0500

    Made storybook portion better

--- src/components/SelectInput/SelectInput.stories.tsx
+++ src/components/SelectInput/SelectInput.stories.tsx
@@ -1,23 +1,24 @@
 import React, {useState} from "react";
 import {Meta} from "@storybook/react/types-6-0";
 import {Story} from "@storybook/react";
-import SelectInput from "./SelectInput";
-import {SelectProps} from "@material-ui/core/Select";
+import SelectInput, {SelectInputProps} from "./SelectInput";
 
 export default {
   title: "Components/SelectInput",
   component: SelectInput,
 } as Meta;
 
-const Template: Story<SelectProps> = (args) => {
+const Template: Story<SelectInputProps> = (args) => {
   return (
-    <SelectInput options={[
-      {label: "None", value: ""},
-      {label: "Milk", value: "Cow"},
-      {label: "OrangeJuice", value: "Tree"},
-    ]} {...args}/>
+    <SelectInput {...args}/>
   );
 }
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  options: [
+    {label: "None", value: ""},
+    {label: "Milk", value: "Milk"},
+    {label: "Orange Juice", value: "Tree"}
+  ]
+};
