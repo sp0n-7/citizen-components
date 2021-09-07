7a60bc6770c5e256ef37bdfbc4b5c6b8b15f080f
Kushan Gupta <kushan.gupta@citizen.com>
2021-09-07 14:02:33 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-import image from "rollup-plugin-image";
 
 const packageJson = require("./package.json");
 
@@ -29,6 +28,5 @@ export default {
     postcss({
         extensions: ['.css']
     }),
-    image()
   ]
 };
