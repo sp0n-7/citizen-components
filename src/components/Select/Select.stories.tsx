5a1cbf2fc73686628018b349d7cebd6a5e3fc52e
= <kevin.finch@citizen.com>
2021-09-21 17:51:31 -0400

    Width of select and notched label

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent} from "react";
-import {Meta} from "@storybook/react/types-6-0";
-import {Story} from "@storybook/react";
-import {SelectInputProps} from "./Select";
+import React, { ChangeEvent } from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import { SelectInputProps } from "./Select";
 import Select from "./Select";
 
 export default {
@@ -10,22 +10,26 @@ export default {
 } as Meta;
 
 const Template: Story<SelectInputProps> = (args) => {
-  const [value, setValue] = React.useState<any>('');
+  const [value, setValue] = React.useState<any>("");
 
-  const handleChange = (event: ChangeEvent<{ name?: string | undefined; value: unknown }>) => {
+  const handleChange = (
+    event: ChangeEvent<{ name?: string | undefined; value: unknown }>
+  ) => {
     setValue(event.target.value as any);
   };
 
   return (
-    <Select onChange={handleChange} value={value} {...args}/>
+    <div style={{ width: "300px" }}>
+      <Select onChange={handleChange} value={value} {...args} />
+    </div>
   );
-}
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
   options: [
-    {label: "None", value: ""},
-    {label: "Milk", value: "Milk"},
-    {label: "Orange Juice", value: "Tree"}
-  ]
+    { label: "None", value: "" },
+    { label: "Milk", value: "Milk" },
+    { label: "Orange Juice", value: "Tree" },
+  ],
 };
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,14 +1,18 @@
-import React, {ChangeEvent} from "react";
-import {makeStyles, withStyles} from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import {SelectProps} from "@material-ui/core/Select";
-import MaterialUISelect from '@material-ui/core/Select';
-import InputBase from '@material-ui/core/InputBase';
-import ArrowDropDownRoundedIcon from '@material-ui/icons/ArrowDropDownRounded';
-import {BootStrapInputStyle, DropdownStyle, SelectStyle} from "./Select.styles";
+import React, { ChangeEvent } from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import { SelectProps } from "@material-ui/core/Select";
+import MaterialUISelect from "@material-ui/core/Select";
+import InputBase from "@material-ui/core/InputBase";
+import ArrowDropDownRoundedIcon from "@material-ui/icons/ArrowDropDownRounded";
+import {
+  BootStrapInputStyle,
+  DropdownStyle,
+  SelectStyle,
+} from "./Select.styles";
+import TextInput from "../TextInput";
 
-const BootstrapInput = withStyles(BootStrapInputStyle)(InputBase);
-const useStyles = makeStyles(DropdownStyle)
+const useStyles = makeStyles(DropdownStyle);
 const SelectStyled = withStyles(SelectStyle)(MaterialUISelect);
 
 export interface option {
@@ -17,33 +21,34 @@ export interface option {
 }
 
 export interface SelectInputProps extends SelectProps {
-  options: option[]
+  options: option[];
 }
 
-export default function Select({options, ...props}: SelectInputProps) {
-
+export default function Select({ options, ...props }: SelectInputProps) {
   const classes = useStyles();
   return (
     <SelectStyled
-      input={<BootstrapInput/>}
+      input={<TextInput notched label="Hey there label" />}
       defaultValue={"None"}
       displayEmpty
       MenuProps={{
-        classes: {paper: classes.dropdown},
+        classes: { paper: classes.dropdown },
         anchorOrigin: {
           vertical: "top",
-          horizontal: "left"
+          horizontal: "left",
         },
         transformOrigin: {
           vertical: "top",
-          horizontal: "left"
+          horizontal: "left",
         },
-        getContentAnchorEl: null
+        getContentAnchorEl: null,
       }}
       IconComponent={ArrowDropDownRoundedIcon}
       {...props}
     >
-      {options.map((pair) => (<MenuItem value={pair.value}>{pair.label}</MenuItem>))}
+      {options.map((pair) => (
+        <MenuItem value={pair.value}>{pair.label}</MenuItem>
+      ))}
     </SelectStyled>
   );
 }
