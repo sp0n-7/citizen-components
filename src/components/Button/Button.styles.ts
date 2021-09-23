311fd820008f284658eaf59e5eec1a38d57aa193
= <kevin.finch@citizen.com>
2021-09-23 17:08:32 -0400

    Tweak buttons to match designs more closely with some variants

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,29 +1,30 @@
 export const ButtonStyle = () => ({
   root: {
-    borderRadius: "30px",
-    padding: '15px  25px 15px 25px',
+    borderRadius: "20px",
   },
   label: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      'sans-serif',
-    ].join(','),
-    fontSize: "13px",
+    fontFamily: ["-apple-system", "BlinkMacSystemFont", "sans-serif"].join(","),
+    fontSize: "12px",
     lineHeight: "16px",
   },
-})
+});
 
-export const PropColors = () => ({
-  colors: (props: {
+export const Customization = () => ({
+  root: (props: {
     backgroundColor?: string;
     textColor?: string;
+    width?: string;
+    height?: string;
+    padding?: string;
   }) => ({
     backgroundColor: props.backgroundColor,
     color: props.textColor,
     "&:hover": {
-      backgroundColor: props.backgroundColor
+      backgroundColor: props.backgroundColor,
     },
     fontWeight: 600,
+    width: props.width,
+    height: props.height,
+    padding: props.padding,
   }),
-})
+});
