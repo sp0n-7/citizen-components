29a26d6349c9608f629ef054318adf8cfe28f0fb
= <kevin.finch@citizen.com>
2021-09-29 13:42:01 -0400

    Add colored checkboxes

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,27 +1,46 @@
-import React from "react"
-import classnames from "classnames"
+import React from "react";
+import classnames from "classnames";
 
-import style from "./Checkbox.module.css"
+import style from "./Checkbox.module.css";
 export interface CheckboxProps {
-  active: boolean
-  handleClick: React.MouseEventHandler<HTMLDivElement>
+  active: boolean;
+  handleClick: React.MouseEventHandler<HTMLDivElement>;
+  blue?: boolean;
 }
 
-export const Checkbox = ({ active, handleClick }: CheckboxProps) => {
+export const Checkbox = ({ active, handleClick, blue }: CheckboxProps) => {
   return (
     <div
       className={classnames(style.box, {
-        [style.active]: active,
+        [style.active]: active && !blue,
+        [style.blueActive]: active && blue,
       })}
       onClick={handleClick}
     >
-      {active &&
-        <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 4.69231L5.2 9L13 1" stroke={"white"} stroke-width="2"/>
+      {active && !blue && (
+        <svg
+          width="14"
+          height="11"
+          viewBox="0 0 14 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 4.69231L5.2 9L13 1" stroke={"white"} stroke-width="2" />
         </svg>
-      }
+      )}
+      {active && blue && (
+        <svg
+          width="14"
+          height="11"
+          viewBox="0 0 14 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 4.69231L5.2 9L13 1" stroke="#4DA6FF" stroke-width="2" />
+        </svg>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Checkbox
+export default Checkbox;
