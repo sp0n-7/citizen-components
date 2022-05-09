62589a65e05158d8886ea39723cc8f4e292f7613
Kushan <kushan.gupta@citizen.com>
2022-05-09 13:58:11 -0400

    new text input who dis

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,24 +1,75 @@
 import React, { useState } from "react";
 
-import { OutlinedInput } from "@mui/material";
-import { OutlinedInputProps } from "@mui/material/OutlinedInput";
-import { makeStyles, withStyles } from "@mui/styles";
+import { TextField } from "@mui/material";
+import { TextFieldProps } from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
 
-import { baseStyles, Customization } from "./TextInput.styles";
+const BaseInput = styled(TextField)`
+  .MuiOutlinedInput-root {
+    height: 44px;
+    width: 100%;
+    font-weight: 500;
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    font-size: 12px;
+    border-color: #333333;
+  }
 
-const useStyles = makeStyles(Customization);
+  .MuiOutlinedInput-notchedOutline {
+    border-color: #333333 !important;
+  }
 
-const BaseInput = withStyles(baseStyles)(OutlinedInput);
+  &.Mui-focused .MuiOutlinedInput-notchedOutline {
+    border-color: #333333 !important;
+  }
 
-const TextInput = (props: OutlinedInputProps) => {
+  &:hover .MuiOutlinedInput-notchedOutline {
+    border-color: #333333 !important;
+  }
+
+  &.Mui-disabled .MuiOutlinedInput-notchedOutline {
+    border-color: #333333 !important;
+    color: #8c8c8c !important;
+  }
+
+  &:hover &.Mui-disabled .MuiOutlinedInput-notchedOutline {
+    border-color: #333333 !important;
+  }
+
+  &::placeholder {
+    text-overflow: ellipsis;
+    color: #8c8c8c !important;
+    opacity: 100;
+    font-size: 12px;
+  }
+
+  & label {
+    visibility: visible;
+    opacity: 1;
+    font-weight: 540;
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    font-size: 13px;
+    line-height: 16px;
+    color: #8c8c8c !important;
+    padding-top: 5px;
+  }
+`;
+
+const TextInput = (props: TextFieldProps) => {
   const [originalValue, setOriginalValue] = useState(props.value);
   const textColor = originalValue !== props.value ? "white" : "#8c8c8c";
 
-  const classes = useStyles({
-    textColor
-  });
-
-  return <BaseInput {...props} classes={{ input: classes.input }} />;
+  return (
+    <BaseInput
+      {...props}
+      sx={{
+        color: textColor,
+        "& .MuiInputBase-input.Mui-disabled": {
+          WebkitTextFillColor: textColor
+        }
+      }}
+      variant="outlined"
+    />
+  );
 };
 
 export default TextInput;
